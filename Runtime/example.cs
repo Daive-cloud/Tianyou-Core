aea09b2ccb94ec530cc021c165fac133fbaa2de8
Daive-cloud <2420638649@qq.com>
2025-08-22 15:30:50 +0800

    Initial commit

---
+++ Runtime/example.cs
@@ -0,0 +1,11 @@
+namespace Tianyou.Core
+{
+    public static class Example
+    {
+        public static void TextMethod()
+        {
+            UnityEngine.Debug.Log("Hello from Tainyou.Core");
+        }
+        
+    }
+}
